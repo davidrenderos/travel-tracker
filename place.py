1448aa82074c2013db6686b2382b09138fdb7a34
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-24 11:09:08 +1000

    Empty place test and pass

--- place.py
+++ place.py
@@ -6,17 +6,22 @@
 
 class Place:
     """What this class does: """
-    def __init__(self):
-        pass
+
+    def __init__(self, name="", country="", priority=0, is_visited=False):
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.is_visited = is_visited
 
     def __str__(self):
-        pass
+        return "{}, {}, {}, {}".format(self.name, self.country, self.priority, self.is_visited)
 
     def visited(self):
         pass
+        # return self.is_visited == True
 
     def unvisited(self):
         pass
 
-    def impoortant_place(self):
+    def important_place(self):
         pass
