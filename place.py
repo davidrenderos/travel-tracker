5a089d4913a1fe257766d14250d2d8ba8aee66fc
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-24 10:55:07 +1000

    Add method place holder

--- place.py
+++ place.py
@@ -1,9 +1,22 @@
-"""..."""
+"""name date etc..."""
 
 
 # Create your Place class in this file
 
 
 class Place:
-    """..."""
-    pass
+    """What this class does: """
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def visited(self):
+        pass
+
+    def unvisited(self):
+        pass
+
+    def impoortant_place(self):
+        pass
--- placecollection.py
+++ placecollection.py
@@ -1,9 +1,24 @@
-"""..."""
+"""name date etc..."""
 
 
 # Create your PlaceCollection class in this file
 
 
 class PlaceCollection:
-    """..."""
-    pass
+    """What this class does: """
+    list_of_places = []
+
+    def load_places(self):
+        pass
+
+    def save_places(self):
+        pass
+
+    def add_place(self):
+        pass
+
+    def get_unvisited(self):
+        pass
+
+    def sort_places(self):
+        pass
