69c885ed208171fbc3ba10a54f9927856d4a7c8d
David Renderos <david.renderos1@my.jcu.edu.au>
2020-09-28 13:22:05 +1000

    Test and pass init method Test and pass add place method

--- placecollection.py
+++ placecollection.py
@@ -6,7 +6,11 @@
 
 class PlaceCollection:
     """What this class does: """
-    list_of_places = []
+    def __init__(self):
+        self.places = []
+
+    def __str__(self):
+        return str([str(place) for place in self.places])
 
     def load_places(self):
         pass
@@ -14,8 +18,8 @@ class PlaceCollection:
     def save_places(self):
         pass
 
-    def add_place(self):
-        pass
+    def add_place(self, place):
+        self.places.append(place)
 
     def get_unvisited(self):
         pass
