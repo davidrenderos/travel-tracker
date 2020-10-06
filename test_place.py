284de0102a6be920e9d393e8f6420fbf596e1db7
David Renderos <david.renderos1@my.jcu.edu.au>
2020-10-06 18:26:56 +1000

    Refactor multiple test

--- test_place.py
+++ test_place.py
@@ -8,7 +8,7 @@ def run_tests():
     # Test empty place (defaults)
     print("Test empty place:")
     default_place = Place()
-    print(default_place)
+    print("{}\n".format(default_place))
     assert default_place.name == ""
     assert default_place.country == ""
     assert default_place.priority == 0
@@ -17,37 +17,37 @@ def run_tests():
     # Test initial-value place
     print("Test initial-value place:")
     new_place = Place("Malagar", "Spain", 1, False)
-    print(new_place)
+    print("{}\n".format(new_place))
     assert new_place.name == "Malagar"
     assert new_place.country == "Spain"
     assert new_place.priority == 1
     assert new_place.is_visited is False
 
-    # Mark if visited
-    print("Mark if visited:")
-    visited_place = Place("Sydney", "Australia", 3, True)
-    print(visited_place)
-    print(Place.mark_visited(visited_place))
-    assert visited_place.is_visited is True
+    # Test mark as visited
+    print("Mark as visited:")
+    unvisited_place = Place("Sydney", "Australia", 3, False)
+    print("Expect False got {}".format(unvisited_place.is_visited))
+    unvisited_place.mark_visited()
+    print("Expect True got {}\n".format(unvisited_place.is_visited))
+    assert unvisited_place.is_visited is True
 
-    # Mark if unvisited
-    print("Mark if unvisited:")
-    unvisited_place = Place("Tokyo", "Japan", 1, False)
-    print(unvisited_place)
-    print(Place.mark_unvisited(unvisited_place))
-    assert unvisited_place.is_visited is False
+    # Test mark as unvisited
+    print("Mark as unvisited:")
+    visited_place = Place("Tokyo", "Japan", 1, True)
+    print("Expect True got {}".format(visited_place.is_visited))
+    visited_place.mark_unvisited()
+    print("Expect False got {}\n".format(visited_place.is_visited))
+    assert visited_place.is_visited is False
 
-    # Test important  place
-    print("Test important place:")
-    important_place = Place("New York", "USA", 4, False)
-    print(important_place)
-    print(Place.is_important_place(important_place))
-
-    # Test unimportant  place
+    # Test unimportant place
     print("Test unimportant place:")
+    important_place = Place("New York", "USA", 4, False)
+    print("Expect False got {}\n".format(Place.is_important_place(important_place)))
+
+    # Test important place
+    print("Test important place:")
     unimportant_place = Place("Madrid", "Spain", 1, False)
-    print(unimportant_place)
-    print(Place.is_important_place(unimportant_place))
+    print("Expect True got {}\n".format(Place.is_important_place(unimportant_place)))
 
 
 run_tests()
