d7c5adb44e78fb359e8ba66e5a9b0cc4a550e062
david <djrenderos@gmail.com>
2020-10-20 11:14:21 +1000

    Add init of load movies Add base build

--- main.py
+++ main.py
@@ -4,14 +4,25 @@ Date:
 Brief Project Description:
 GitHub URL:
 """
-# Create your main program in this file, using the TravelTrackerApp class
 
 from kivy.app import App
+from placecollection import PlaceCollection
+from kivy.lang import Builder
+
+PLACES_FILE = 'places.csv'
 
 
 class TravelTrackerApp(App):
     """..."""
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.place_collection = PlaceCollection()
+        self.place_collection.load_places(PLACES_FILE)
+
+    def build(self):
+        self.title = "Places To Visit 2.0"
+        self.root = Builder.load_file('app.kv')
+        return self.root
 
 
 if __name__ == '__main__':
