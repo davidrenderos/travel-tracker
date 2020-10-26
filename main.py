819f4594a6d78bdabd197419b1e0868be540b781
david <djrenderos@gmail.com>
2020-10-26 12:26:30 +1000

    Base of GUI implemented Spinner functional

--- main.py
+++ main.py
@@ -8,12 +8,17 @@ GitHub URL:
 from kivy.app import App
 from placecollection import PlaceCollection
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty
 
 PLACES_FILE = 'places.csv'
+SORT_CATEGORIES = ['name', 'country', 'priority', 'is visited']
 
 
 class TravelTrackerApp(App):
     """..."""
+    current_selection = StringProperty()
+    sort_by = ListProperty()
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.place_collection = PlaceCollection()
@@ -22,6 +27,8 @@ class TravelTrackerApp(App):
     def build(self):
         self.title = "Places To Visit 2.0"
         self.root = Builder.load_file('app.kv')
+        self.sort_by = sorted(SORT_CATEGORIES)
+        self.current_selection = self.sort_by[0]
         return self.root
 
 
